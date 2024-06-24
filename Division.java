88b63f8432d56569159c7d63eed756e2eca99496
Your Name <you@example.com>
2024-06-24 20:50:38 +0200

    oppp

---
+++ Division.java
@@ -0,0 +1,20 @@
+public class Division extends Operation {
+    
+    public Division(Expression op1, Expression op2) {
+        super(op1, op2);
+    }
+     
+    public double valeur() {
+        double operande1 = this.getOperande1().valeur();
+        double operande2 = this.getOperande2().valeur();
+        if (operande2 == 0.0) {
+            System.out.println("\nDivision par zéro impossible.");
+            throw new ArithmeticException("Division par zéro");
+        }
+        return operande1 / operande2;
+    }
+    
+    public String toString() {
+        return "(" + this.getOperande1().toString() + " / " + this.getOperande2().toString() + ")";
+    }
+}
